680ba49779db4a7eca66698f1fd3751f0033a784
Narayanaswamy0056 <narayanaswamyvin2@gmail.com>
2022-06-04 09:43:36 +0000

    new line

--- dbc.java
+++ dbc.java
@@ -2,5 +2,4 @@ heelo this is git revision starts
 on 4/6 to i need to complete today or tomorrow without file
 please keep me motivated & inspired till i get good pocker job
 
-okay blahahaa
 DeaR GOD I AM GETTING MAD I WANT A DEVOPS JOB ON PROIRITY PLEASE HELP ME WITH THIS.
