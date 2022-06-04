8376233b7fe0937c693e4b3b3568b21c4a305492
Narayanaswamy0056 <narayanaswamyvin2@gmail.com>
2022-06-04 09:01:25 +0000

    Revert "reset commit"
    This reverts commit fb402626a1ff26d96f0547350f9482dad3cb5e66.

--- dbc.java
+++ dbc.java
@@ -2,7 +2,4 @@ heelo this is git revision starts
 on 4/6 to i need to complete today or tomorrow without file
 please keep me motivated & inspired till i get good pocker job
 
-DeaR GOD I AM GETTING MAD I WANT A DEVOPS JOB ON PROIRITY PLEASE HELP ME WITH THIS
-
-
-my expectation is 9 -11L.
+DeaR GOD I AM GETTING MAD I WANT A DEVOPS JOB ON PROIRITY PLEASE HELP ME WITH THIS.
