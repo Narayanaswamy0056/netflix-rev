c278da3d0a370d5ab64397d492493d3ddca54fb9
Narayanaswamy0056 <narayanaswamyvin2@gmail.com>
2022-06-04 08:46:53 +0000

    updated commit

--- dbc.java
+++ dbc.java
@@ -1,3 +1,5 @@
 heelo this is git revision starts 
 on 4/6 to i need to complete today or tomorrow without file
 please keep me motivated & inspired till i get good pocker job
+
+DeaR GOD I AM GETTING MAD I WANT A DEVOPS JOB ON PROIRITY PLEASE HELP ME WITH THIS.
