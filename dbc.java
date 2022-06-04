fb402626a1ff26d96f0547350f9482dad3cb5e66
Narayanaswamy0056 <narayanaswamyvin2@gmail.com>
2022-06-04 09:00:17 +0000

    reset commit

--- dbc.java
+++ dbc.java
@@ -2,4 +2,7 @@ heelo this is git revision starts
 on 4/6 to i need to complete today or tomorrow without file
 please keep me motivated & inspired till i get good pocker job
 
-DeaR GOD I AM GETTING MAD I WANT A DEVOPS JOB ON PROIRITY PLEASE HELP ME WITH THIS.
+DeaR GOD I AM GETTING MAD I WANT A DEVOPS JOB ON PROIRITY PLEASE HELP ME WITH THIS
+
+
+my expectation is 9 -11L.
